6e62083a8bb5acaae905faedce0994a47ca19cf0
Eunice Cruz <sekruz@gmail.com>
2022-11-15 20:05:15 -0600

    Timeout e Interval en juego maestro

--- src/app/components/juego/juego.component.ts
+++ src/app/components/juego/juego.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ItemService } from 'src/app/services/item.service';
 import { ItemdetService } from 'src/app/services/itemdet.service';
@@ -15,24 +15,16 @@ import { Item } from 'src/app/interfaces/item';
   styleUrls: ['./juego.component.css']
 })
 export class JuegoComponent implements OnInit {
-  //createConfiguracion: FormGroup;
   juegos: any[] = [];
-  //Configuracion: any[] = [];
-  //item: any[] = [];
-  items: Item[] = [];
-  Configuracions: Configuracion[] = [];
-  Configuracion:  any | Configuracion;
-  idConfiguracion = '';
-  //preguntas: any[] = [];
+  Configuracion: any | Configuracion;
   preguntas: any[] = [];
-  preguntaActual:any;
+  preguntaActual: any;
   respuestas: any[] = [];
-  //createJuego: FormGroup;
-  submitted = false;
   loading = false;
   id: string | null;
-  private indiceActual:number = -1;
-  titulo = 'Armar Juego';
+  private indiceActual: number = -1;
+  seconds!: number;
+  intervalId!: number;
 
   constructor(private fb: FormBuilder,
     private _ConfiguracionService: ConfiguracionService,
@@ -41,86 +33,110 @@ export class JuegoComponent implements OnInit {
     private toastr: ToastrService,
     private aRoute: ActivatedRoute,
     private router: Router) {
-      //this.idConfiguracion =this.router.getCurrentNavigation().extras.state.id;
 
+    this.resetSecs();
     this.id = this.aRoute.snapshot.paramMap.get("id");
-    // this.idItem = this.aRoute.snapshot.paramMap.get("id_item");
-    // console.log(this.id);
-     }
+  }
+
 
   ngOnInit(): void {
-     // this._ConfiguracionService.getConfiguracions().subscribe(
-    //   prod => {
-    //     console.log(prod);
-    //   }
-    //)
-   // this.getConfiguracionByIdConfiguracion();
-   this.getItemByIdConfiguracion();
-   //this.getItemdetByIdItem();
+    this.getItemByIdConfiguracion();
   }
+  private resetSecs() {
+    this.seconds = 3;
+  }
+  private conteoRegresivo(): void {
+
+    if (--this.seconds >= 0) {
+      console.log('quedan ', this.seconds + 1);
+      clearInterval(this.intervalId);
+    }
+  }
+
+  contador() {
+    this.resetSecs();
+    setInterval(() => this.conteoRegresivo(), 1000);
+    let i = 0;
+
+    for (i = 0; i < this.preguntas.length; i++) {
+      this.doSetTimeout(i + 1, 'test');
+      this.cambiarEstadoTimeOut(i + 1);
+    }
+  }
+
+  doSetTimeout(ciclo: number, valor: string) {
+
+    const name = () => {
+      this.siguientePregunta()
+      console.log('ciclo siguientePregunta', ciclo);
+      this.resetSecs();
+      setInterval(() => this.conteoRegresivo(), (ciclo + 1) * 1000);
+    };
+    setTimeout(function () {
+      name()
+    }, (ciclo * this.seconds) * 1000); //1000 = time in milliseconds
+  }
+
+  cambiarEstadoTimeOut(ciclo: number) {
+
+    const name = () => {
+      console.log('metodo para cambiar estado');
+      if (this.indiceActual < this.preguntas.length) {
+        this.preguntaActual = this.preguntas[this.indiceActual];
+        this._ItemService.actualizarItem()
+        //console.log(this.preguntaActual );
+        //console.log(this.preguntas[this.indiceActual+1]);
+        this._ItemDetService.getItemdetByIdItem(this.preguntaActual.id_item).subscribe(data => {
+          this.respuestas = data;
+        });
+      }
+      //this.siguientePregunta()
+     // console.log('ciclo', ciclo);
+      //this.resetSecs();
+      //setInterval(() => this.conteoRegresivo(), (ciclo + 1) * 1000);
+    };
+    setTimeout(function () {
+      name()
+    }, (ciclo * this.seconds) * 1000); //1000 = time in milliseconds
+  }
+
   getItemByIdConfiguracion() {
-    // console.log(this.id);
-    //let identificador = this.id;
     if (this.id != null) {
       this._ConfiguracionService.getConfiguracion(this.id).subscribe(
         (Configuracion) => {
           this.Configuracion = Configuracion;
-          this._ItemService.getItemByIdConfiguracion(Configuracion.id_configuracion,0).subscribe(
+          this._ItemService.getItemByIdConfiguracion(Configuracion.id_configuracion, 0).subscribe(
             (item: any) => {
               this.preguntas = item;
               this.siguientePregunta();
-          }
+            }
           )
         }
       )
     }
   }
 
-  baseRandom(lower: number, upper: number) {
-    return 1;
-     //return lower + nativeFloor(nativeRandom() * (upper - lower + 1));
-  }
-
   siguientePregunta(): void {
     this.indiceActual++;
-    console.log(this.indiceActual);
-    console.log(this.preguntas.length);
-       
-    if(this.indiceActual < this.preguntas.length){
+    //console.log('siguientePregunta--indiceActual', this.indiceActual);
+    //console.log('siguientePregunta--this.preguntas.length)', this.preguntas.length);
+
+    if (this.indiceActual < this.preguntas.length) {
       this.preguntaActual = this.preguntas[this.indiceActual];
+      //console.log(this.preguntaActual );
+      //console.log(this.preguntas[this.indiceActual+1]);
       this._ItemDetService.getItemdetByIdItem(this.preguntaActual.id_item).subscribe(data => {
-          this.respuestas = data;
+        this.respuestas = data;
       });
     }
-    
-  }
 
-  // getConfiguracionByIdConfiguracion() {
-  //   // console.log(this.idConfiguracion);
-  //   let identificador = this.id;
-  //   if (identificador != null) {
-  //     this._ConfiguracionService.getConfiguracion(identificador).subscribe(
-  //       (part) => {
-  //         console.log(part);
-  //         //obtiene el id_Configuracion del documento(id) recibido
-  //         this._ConfiguracionService.getConfiguracionByIdConfiguracion(part.id_Configuracion).subscribe(
-  //           (Configuracion) => {
-  //             console.log(Configuracion);
-  //             this.preguntas = Configuracion;
-  //             // this.Configuracions = part;
-  //           }
-  //         )
-  //       }
-  //     )
-  //   }
-  // }
+  }
 
   getConfiguracion() {
     if (this.id !== null) {
       this.loading = true;
       this._ConfiguracionService.getConfiguracion(this.id).subscribe(data => {
         this.loading = false;
-        // console.log(data.payload.data()['id_Configuracion']); //accedemos a todos los datos
         this.juegos.push({
           id_Configuracion: data.payload.doc.data()['id_Configuracion']
         })
@@ -132,12 +148,12 @@ export class JuegoComponent implements OnInit {
     if (this.id != null) {
       this._ConfiguracionService.getConfiguracion(this.id).subscribe(
         (Configuracion) => {
-          this._ItemService.getItemByIdConfiguracion(Configuracion.id_configuracion,0).subscribe(
+          this._ItemService.getItemByIdConfiguracion(Configuracion.id_configuracion, 0).subscribe(
             (item) => {
               item.forEach((element: any) => {
                 this._ItemDetService.getItemdetByIdItem(element.payload.doc.id_item).subscribe(
                   (itemdet) => {
-                    console.log(itemdet);
+                    console.log('getItemdetByIdItem--itemdet', itemdet);
                     this.respuestas = itemdet;
                   }
                 )
@@ -149,28 +165,5 @@ export class JuegoComponent implements OnInit {
       )
     }
   }
-
-  // getConfiguracion(){
-  //   if(this.id !== null)  {
-  //     this.loading = true;
-  //     this._ConfiguracionService.getConfiguracion(this.id).subscribe(data =>{
-  //       this.loading = false;
-  //       console.log(data.payload.data()['id_Configuracion']); //accedemos a todos los datos       
-  //     })
-  //   }
-  //   this._ConfiguracionService.getConfiguracions().subscribe(data => {
-  //     this.Configuracion = [];
-  //     data.forEach((element:any )=> {
-  //       // console.log(element.payload.doc.id);
-  //       /*console.log(element.payload.doc.data());*/
-  //       this.Configuracion.push({
-  //         id : element.payload.doc.id,
-  //         ...element.payload.doc.data()
-  //       })
-  //     });
-  //     console.log(this.Configuracion);
-  //   }
-  //   )
-  // }
 
 }
